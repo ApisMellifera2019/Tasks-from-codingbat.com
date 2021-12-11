12ea522f0d234579138336f0c9fd5114cb1169b9
Vadym Z <prohtehb@gmail.com>
2021-12-11 20:26:23 +0200

    first task from Codingbat

---
+++ Task1.java
@@ -0,0 +1,18 @@
+package Codingbat;
+
+public class Task1 {
+    public static void main(String[] args) {
+
+
+    }
+
+
+    public boolean sleepIn(boolean weekday, boolean vacation) {
+
+        if(weekday == true && vacation == false) {
+            return false;
+        }else
+            return true;
+    }
+
+}
